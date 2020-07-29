aa5c42e3a07627433a7f6008a96fc581c5470d60
Max zhang <373884384@qq.com>
2020-07-29 16:28:10 +0800

    一键生成CURD

---
+++ app/common/curdgener/exceptions/FileException.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace app\common\curdgener\exceptions;
+
+class FileException extends \Exception
+{
+
+}
---
+++ app/common/curdgener/exceptions/TableException.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\common\curdgener\exceptions;
+
+
+use Exception;
+
+class TableException extends Exception
+{
+
+}
