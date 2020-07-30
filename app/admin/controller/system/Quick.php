b8a48e2623da7470d103dc8850442b61a915b02f
Max zhang <373884384@qq.com>
2020-07-30 09:20:25 +0800

    后台信息配置

---
+++ app/admin/controller/system/Quick.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\admin\controller\system;
+
+
+use app\admin\model\SystemQuick;
+use app\common\annotation\ControllerAnnotation;
+use app\common\controller\AdminController;
+use app\common\annotation\NodeAnotation;
+use think\App;
+
+/**
+ * @ControllerAnnotation(title="快捷入口管理")
+ * Class Quick
+ * @package app\admin\controller\system
+ */
+class Quick extends AdminController
+{
+
+    use \app\admin\traits\Curd;
+
+    protected $sort = [
+        'sort' => 'desc',
+        'id'   => 'desc',
+    ];
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->model = new SystemQuick();
+    }
+
+}
