25dd711f3375fd1f098c4224e09e98342b99e326
Max zhang <373884384@qq.com>
2020-07-30 09:22:40 +0800

    后台信息配置

---
+++ app/admin/model/SystemAuthNode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\admin\model;
+
+
+use app\common\model\TimeModel;
+
+class SystemAuthNode extends TimeModel
+{
+
+}
---
+++ app/admin/model/SystemQuick.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\admin\model;
+
+
+use app\common\model\TimeModel;
+
+class SystemQuick extends TimeModel
+{
+
+    protected $deleteTime = 'delete_time';
+
+}
